d62bfda55fb419fe8af83996ccdb707f1fd2be0a
Brian McGue <mcgue.brian@gmail.com>
2014-08-07 12:08:54 -0700

    Allow multiple searches.

--- craigslist.py
+++ craigslist.py
@@ -12,30 +12,34 @@ def notify(title, subtitle, message, open):
 def main():
 	
 	##### THIS IS THE LINE YOU REPLACE. EVERYTHING BETWEEN THE DOUBLE QUOTES ('')
-	mainSearch = 'http://www.craigslist.com/url-to-search'
+	mainSearches = [
+		'http://www.craigslist.com/url-to-search',
+		'http://www.craigslist.com/another-url-if-you-want',
+	]
 	#####
 	
-	firstResponse = urllib2.urlopen(mainSearch)
-	html1 = firstResponse.read()
-	mainSoup = BeautifulSoup(html1)
-	now = time.localtime()
-	for link in mainSoup.findAll('a',{'class':'i'}):
-		try:
-			href = 'http://sfbay.craigslist.org' + link.get('href')
-			linkResponse = urllib2.urlopen(href)
-			aptHTML = linkResponse.read()
-			aptSoup = BeautifulSoup(aptHTML)
-			postTime = time.strptime(aptSoup.time.get('datetime'), '%Y-%m-%dT%H:%M:%S-0700')
-			minutesAgo = (time.mktime(now) - time.mktime(postTime)) / 60
-			if minutesAgo < 30:
-				notify(
-					title='Found an apartment!',
-					subtitle=href,
-					message=aptSoup.findAll('h2',{'class':'postingtitle'})[0].get_text().strip(),
-					open=href
-				)
-		except Exception as e:
-			pass
+	for search in mainSearches:
+		firstResponse = urllib2.urlopen(search)
+		html1 = firstResponse.read()
+		mainSoup = BeautifulSoup(html1)
+		now = time.localtime()
+		for link in mainSoup.findAll('a',{'class':'i'}):
+			try:
+				href = 'http://sfbay.craigslist.org' + link.get('href')
+				linkResponse = urllib2.urlopen(href)
+				aptHTML = linkResponse.read()
+				aptSoup = BeautifulSoup(aptHTML)
+				postTime = time.strptime(aptSoup.time.get('datetime'), '%Y-%m-%dT%H:%M:%S-0700')
+				minutesAgo = (time.mktime(now) - time.mktime(postTime)) / 60
+				if minutesAgo < 30:
+					notify(
+						title='Found an apartment!',
+						subtitle=href,
+						message=aptSoup.findAll('h2',{'class':'postingtitle'})[0].get_text().strip(),
+						open=href
+					)
+			except Exception as e:
+				pass
 
 if __name__ == '__main__':
 	main()
